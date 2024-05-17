36cbc8b710af46396a2ad933b558b428401d5945
BITTU KUMAR <169143833+BittuSotfCoder@users.noreply.github.com>
2024-05-18 01:56:38 +0530

    al

--- lib/ClassesLibrary/Res.dart
+++ lib/ClassesLibrary/Res.dart
@@ -1,7 +1,7 @@
 // ignore: file_names
 class Res {
   String getString(String action) {
-    String link = "http://nodejsapidata.onrender.com/$action";
+    String link = "http://192.168.1.40/API/jsonDataInsert.php?action=$action";
     return link;
   }
     String setString(String action) {
